5f6edd2bc0bc33a8c9a520fd76c0f58f4c8fe683
MoathDawahreh <moath.dawahreh@yahoo.com>
2017-02-03 21:14:48 +0200

    -

--- fizzbuzz-fozzbazz/app.js
+++ fizzbuzz-fozzbazz/app.js
@@ -18,6 +18,32 @@ angular.module('app', ['ngRoute'])
 /*  HINT: Make sure your controllers, methods, and variables 
     are named what $routeProvider and the partials are expecting  */
 
-  .factory('counter', function(){})
-  .controller('fizzbuzzCtrl', function(){})
-  .controller('fozzbazzCtrl', function(){});
+  .factory('counter', function(){
+    var counter = 0
+    $routeProvider.$on('',function(){
+      return counter++
+    })
+  })
+  .controller('fizzbuzzCtrl', function(){
+    if ($routeProvider.counter%3===0) {
+      console.log('Fizz')
+    }
+    if ($routeProvider.counter%5===0) {
+      console.log('Buzz')
+    }
+    if ($routeProvider.counter%5===0 &&$routeProvider.counter%3 ===0) {
+      console.log('FIZZBUZZ')
+    }
+  })
+
+  .controller('fozzbazzCtrl', function(){
+       if ($routeProvider.counter%4===0) {
+      console.log('FOZZ')
+    }
+    if ($routeProvider.counter%6===0) {
+      console.log('BAZZ')
+    }
+    if ($routeProvider.counter%4===0 &&$routeProvider.counter%6 ===0) {
+      console.log('FOZZBAZZ')
+    }
+  });
--- node-express-sequelize/server.js
+++ node-express-sequelize/server.js
@@ -6,9 +6,29 @@ var sequelize = new Sequelize('test', 'test', null, { dialect: 'sqlite', storage
 var User = sequelize.define('User', {
   username: Sequelize.STRING
 });
-
 /*  Create a '/users' route that responds to 
     a GET request with all users in the database */
+    app.get('/users',function(req,rs){
+    	res.render('')
+    })
+    app.post('/users',function(req,res){
+  //   	var username = req.body.username
+	 //    new User = sequelize.define('user', {
+	 //  		username: username,
+		// });
+
+		sequelize.sync().then(function() {
+		  return User.create({
+		    username: '',
+		  });
+		}).then(function() {
+		  console.log(username.get({
+		    force: true
+		  }));
+		});
+
+    })
+
 
 
 
@@ -16,3 +36,4 @@ module.exports = {
   app: app,
   User: User
 };
+
